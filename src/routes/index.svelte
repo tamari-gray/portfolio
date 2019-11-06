8f5ce316c7420c51c09de31482fb3b253beea5e0
tamari <tamarigray97@gmail.com>
2019-11-07 12:36:53 +1300

    three js

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,76 @@
+<script>
+  import { onMount } from "svelte";
+  import * as THREE from "three";
+
+  onMount(() => {
+    initThreeJs();
+  });
+
+  function initThreeJs() {
+    var camera, scene, renderer;
+    var geometry, material, mesh;
+
+    init();
+    animate();
+
+    function init() {
+      camera = new THREE.PerspectiveCamera(
+        70,
+        window.innerWidth / window.innerHeight,
+        0.01,
+        10
+      );
+      camera.position.z = 1;
+
+      scene = new THREE.Scene();
+
+      geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+      material = new THREE.MeshNormalMaterial();
+
+      mesh = new THREE.Mesh(geometry, material);
+      scene.add(mesh);
+
+      renderer = new THREE.WebGLRenderer({ antialias: true });
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      document
+        .querySelector(".svelte-1uhnsl8")
+        .setAttribute("style", "padding: 0;");
+      renderer.domElement.setAttribute("style", "width: 100vw; height: 100vh");
+      document.querySelector(".scene").appendChild(renderer.domElement);
+    }
+
+    function animate() {
+      requestAnimationFrame(animate);
+
+      mesh.rotation.x += 0.01;
+      mesh.rotation.y += 0.02;
+
+      renderer.render(scene, camera);
+    }
+  }
+</script>
+
 <style>
+  .scene {
+    position: absolute;
+    z-index: 1;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    bottom: 0;
+    height: 100%;
+  }
+
   .hero {
-    height: 80vh;
     display: flex;
     flex-direction: row;
     flex-wrap: wrap;
     justify-content: center;
     align-content: center;
+    height: 80vh;
+    position: relative;
+    z-index: 2;
+    color: antiquewhite;
   }
 
   h1 {
@@ -37,12 +102,11 @@
   <title>Tamari gray</title>
 </svelte:head>
 
+<div class="scene" />
 <div class="hero">
-
   <h1>
     Im a
     <span>full stack developer</span>
   </h1>
   <h3>=> making cool stuff is fun :)</h3>
-
 </div>
